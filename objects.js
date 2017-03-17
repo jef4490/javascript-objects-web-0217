b814d72f688c6a0e8aa180170eda56e583c92aa4
jef4490 <jeffrey.hechler@gmail.com>
2017-03-16 20:46:03 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { "Rush": "Tom Sawyer", "The Who": "Baba O'Riley"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
